fcb8c722fa210d1239f12059457d500a136e66ed
Lyz03 <lizoe@netcourrier.com>
2021-11-17 18:14:58 +0100

    exo 69 v2

--- exo 2/app.js
+++ exo 2/app.js
@@ -4,8 +4,7 @@ let newItem = document.createElement('li');
 newItem.innerText = "une salade";
 list.append(newItem);
 
-
-
+// get what the user wrote in the input and put it in the list
 function getValue() {
     let inputTextValue = document.getElementById('input_text').value;
     let item = document.createElement('li');
@@ -18,9 +17,11 @@ let button = document.getElementById('button');
 
 button.addEventListener("click", getValue);
 
-
+//remove the last item from the list if there is one
 function deleteItem() {
-    list.lastChild.remove();
+    if (list.lastChild) {
+        list.lastChild.remove();
+    }
 }
 
 let deleteButton = document.getElementById('delete');
